c128862d686a8e3c0994b6ccdfa6bbd62e814337
Alimalpara <work.alimalpara@gmail.com>
2023-03-07 05:06:08 -0500

    Update script.js

--- Javascript/fbexercise/script.js
+++ Javascript/fbexercise/script.js
@@ -1,29 +1,42 @@
-var user = {
-    username: "Ali",
-    password: "1234567",
-};
+// var user = {
+//     username: "Ali",
+//     password: "1234567",
+// };
 
-var database = [
-    user
-];
+// var database = [
+//     user
+// ];
 
-var newsfeed = [{username: "Ali", timeline:"Learning javascript"},
-{username: "Bob", timeline:"Hey it's chill out there"},
-{username: "Alice", timeline:"Won't get tired of learning"},
-];
+// var newsfeed = [{username: "Ali", timeline:"Learning javascript"},
+// {username: "Bob", timeline:"Hey it's chill out there"},
+// {username: "Alice", timeline:"Won't get tired of learning"},
+// ];
 
-var userNamePrompt = prompt("Please enter your username");
-var userPasswordPrompt = prompt("Please enter your Password");
+// var userNamePrompt = prompt("Please enter your username");
+// var userPasswordPrompt = prompt("Please enter your Password");
 
-function checkUser(username,password){
+// function checkUser(username,password){
 
-    if(username === database[0].username && password === database[0].password){
-        console.log(newsfeed);
-    }else{
-        console.error("Incorrect username/passsword");
+//     if(username === database[0].username && password === database[0].password){
+//         console.log(newsfeed);
+//     }else{
+//         console.error("Incorrect username/passsword");
 
-    }
+//     }
 
+// }
+
+// checkUser(userNamePrompt,userPasswordPrompt);
+
+
+var checkArray = [1,2,3,4,5];
+
+for(var i = 0; i<checkArray.length;i++){
+    console.log(i, checkArray[i]);
 }
 
-checkUser(userNamePrompt,userPasswordPrompt);
+function todoForEach(i,j){
+    console.log(i,j);
+}
+
+checkArray.forEach(todoForEach);

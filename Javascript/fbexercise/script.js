5f325afdb7e5d334de916855e0064ee0b9f37d8a
Alimalpara <work.alimalpara@gmail.com>
2023-03-07 16:51:41 -0500

    Added dom folder
    Dom folders added

--- Javascript/fbexercise/script.js
+++ Javascript/fbexercise/script.js
@@ -1,23 +1,45 @@
-// var user = {
-//     username: "Ali",
-//     password: "1234567",
-// };
+var user = {
+    username: "Ali",
+    password: "1234567",
+};
 
-// var database = [
-//     user
-// ];
+var database = [
+    {
+    username: "Ali",
+    password: "1234567",
+},
+ {
+    username: "Ali2",
+    password: "1234567",
+},
+ {
+    username: "Ali3",
+    password: "1234567",
+}
+];
 
-// var newsfeed = [{username: "Ali", timeline:"Learning javascript"},
-// {username: "Bob", timeline:"Hey it's chill out there"},
-// {username: "Alice", timeline:"Won't get tired of learning"},
-// ];
+var newsfeed = [{username: "Ali", timeline:"Learning javascript"},
+{username: "Bob", timeline:"Hey it's chill out there"},
+{username: "Alice", timeline:"Won't get tired of learning"},
+];
 
-// var userNamePrompt = prompt("Please enter your username");
-// var userPasswordPrompt = prompt("Please enter your Password");
+var userNamePrompt = prompt("Please enter your username");
+var userPasswordPrompt = prompt("Please enter your Password");
+
+//first method
+// function isUserValid(username,password){
+//     for(var i = 0; i< database.length; i++){
+//     if(database[i].username===username && database[i].password === password){
+//         return true;
+//     }
+//     }
+    
+//     return false;
+// }
 
 // function checkUser(username,password){
 
-//     if(username === database[0].username && password === database[0].password){
+//     if(isUserValid(username,password)){
 //         console.log(newsfeed);
 //     }else{
 //         console.error("Incorrect username/passsword");
@@ -29,14 +51,37 @@
 // checkUser(userNamePrompt,userPasswordPrompt);
 
 
-var checkArray = [1,2,3,4,5];
+//second method 
+ function isUserValid(uname,pw){
+    for(var i =0; i < database.length; i++){
+        if(database[i].username === uname && database[i].password === pw){
+            return true;
+        }
+    }
 
-// for(var i = 0; i<checkArray.length;i++){
-//     console.log(i, checkArray[i]);
-// }
+    return false;
 
-function todoForEach(i,j){
-    console.log(i,j);
 }
 
-checkArray.forEach(todoForEach);
+function checkIfUserExists(username,password){
+    if(isUserValid(username,password)){
+        console.log(newsfeed);
+    }else{
+        console.error("OOps");
+    }
+
+}
+checkIfUserExists(userNamePrompt,userPasswordPrompt);
+
+
+// var checkArray = [1,2,3,4,5];
+
+// // for(var i = 0; i<checkArray.length;i++){
+// //     console.log(i, checkArray[i]);
+// // }
+
+// function todoForEach(i,j){
+//     console.log(i,j);
+// }
+
+// checkArray.forEach(todoForEach);
